f751d79ed8a78d02750fc182baccccf5c872488a
ArsenAjiev <selftcoder@gmail.com>
2022-10-28 10:55:51 +0300

    second

--- discography/models.py
+++ discography/models.py
@@ -3,12 +3,12 @@ from django.db import models
 
 class Album(models.Model):
     name = models.CharField(max_length=255)
-    artist = models.CharField(max_length=255)
+    # artist = models.CharField(max_length=255)
     # add new attribute
-    artist_link = models.ForeignKey('Artist', null=True, on_delete=models.CASCADE)
+    artist = models.ForeignKey('Artist', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f' name: {self.name} - artist: {self.artist}'
+        return f' name: {self.name} - artist: {self.artist.name}'
 
 
 # add new related model
